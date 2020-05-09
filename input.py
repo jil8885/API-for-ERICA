e39ee964ac63fc1871beff73542c13b6500193fe
Jeongin Lee <jil8885@hanyang.ac.kr>
2020-05-09 16:52:45 +0900

    Update Cafe : 200509-2

--- input.py
+++ input.py
@@ -1,22 +1,49 @@
 import json
 
-with open('cafe.json') as json_file:
-    data = json.load(json_file)
+def cafe():
+    with open('cafe.json') as json_file:
+        data = json.load(json_file)
 
-while True:
-    name = input("가게 이름 입력: ")
-    if name in data.keys():
-        continue
-    elif name != "":
-        data[name] = {}
-        phone = input("전화 번호 입력: ")
-        place = input("장소 링크 입력: ")
-        if phone:
-            data[name]['phone'] = phone
-        if place:
-            data[name]['place'] = place
-    else:
-        break
+    while True:
+        name = input("가게 이름 입력: ")
+        if name in data.keys():
+            continue
+        elif name != "":
+            data[name] = {}
+            phone = input("전화 번호 입력: ")
+            place = input("장소 링크 입력: ")
+            if phone:
+                data[name]['phone'] = phone
+            if place:
+                data[name]['place'] = place
+        else:
+            break
 
-with open('cafe.json', 'w', encoding='UTF-8') as file:
-    file.write(json.dumps(data, ensure_ascii=False))
+    with open('cafe.json', 'w', encoding='UTF-8') as file:
+        file.write(json.dumps(data, ensure_ascii=False))
+
+
+def food():
+    with open('restaurant.json') as json_file:
+        data = json.load(json_file)
+
+    while True:
+        name = input("가게 이름 입력: ")
+        if name in data.keys():
+            continue
+        elif name != "":
+            data[name] = {}
+            phone = input("전화 번호 입력: ")
+            place = input("장소 링크 입력: ")
+            data[name]['cat'] = input("분류 입력: ")
+            data[name]['menu'] = input("메뉴 입력: ")
+            if phone:
+                data[name]['phone'] = phone
+            if place:
+                data[name]['place'] = place
+        else:
+            break
+
+    with open('restaurant.json', 'w', encoding='UTF-8') as file:
+        file.write(json.dumps(data, ensure_ascii=False))
+food()
