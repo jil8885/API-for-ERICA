5ed43cb660cf1b83f75c5e168c6f32e307f7975b
Jeongin Lee <jil8885@hanyang.ac.kr>
2021-08-13 10:28:07 +0900

    전철 시간표를 json으로 변환하는 구문 수정

--- metro.py
+++ metro.py
@@ -4,13 +4,13 @@ import json
 from openpyxl.utils import column_index_from_string
 
 
-def dump_timetable():
+def dump_timetable_suin():
     workbook = openpyxl.load_workbook('C:\\Users\\Jeongin\\Downloads\\수인분당선.xlsx', read_only=True, data_only=True)
     result = {"weekdays": {"up": [], "down": []}, "weekend": {"up": [], "down": []}}
 
-    heading_column = "C"
-    worksheet_information = [("평일상행", "Z", "weekdays", "up"), ("평일하행", "AW", "weekdays", "down"),
-                             ("휴일상행", "Y", "weekend", "up"), ("평일하행", "AU", "weekend", "down")]
+    heading_column = "B"
+    worksheet_information = [("평일상행", "AS", "weekdays", "up"), ("평일하행", "CJ", "weekdays", "down"),
+                             ("휴일상행", "AS", "weekend", "up"), ("휴일하행", "CJ", "weekend", "down")]
 
     for sheet, column, key1, key2 in worksheet_information:
         worksheet = workbook[sheet]
@@ -24,4 +24,26 @@ def dump_timetable():
     with open('subway/suinline.json', 'w', encoding='utf-8') as f:
         json.dump(result, f, indent='\t', ensure_ascii=False)
 
-dump_timetable()
+
+def dump_timetable_4():
+    workbook = openpyxl.load_workbook('C:\\Users\\Jeongin\\Downloads\\안산과천선.xlsx', read_only=True, data_only=True)
+    result = {"weekdays": {"up": [], "down": []}, "weekend": {"up": [], "down": []}}
+
+    heading_column = "B"
+    worksheet_information = [("평일상행", "S", "weekdays", "up"), ("평일하행", "CG", "weekdays", "down"),
+                             ("휴일상행", "S", "weekend", "up"), ("휴일하행", "CG", "weekend", "down")]
+
+    for sheet, column, key1, key2 in worksheet_information:
+        worksheet = workbook[sheet]
+        for row_index, row in enumerate(worksheet.rows):
+            if row_index > 2:
+                if row[column_index_from_string(column) - 1].value and str(row[column_index_from_string(column) - 1].value).strip():
+                    result[key1][key2].append({"endStn": row[column_index_from_string(heading_column) - 1].value,
+                                               "time": str(row[column_index_from_string(column) - 1].value)})
+
+    with open('subway/4.json', 'w', encoding='utf-8') as f:
+        json.dump(result, f, indent='\t', ensure_ascii=False)
+
+
+dump_timetable_suin()
+dump_timetable_4()
